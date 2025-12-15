a750ab44b5a118c134bceb9707d699a728f52b4d
christianfrech <64506602+christianfrech@users.noreply.github.com>
2025-12-15 01:42:45 +0000

    Deploying to gh-pages from @ christianfrech/christianfrechsite.github.io@a45b455caee39f001dc12faaf618f566f30b868f 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "christianfrech/christianfrech.github.io",
+    "data-repo": "christianfrech/christianfrechsite.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
